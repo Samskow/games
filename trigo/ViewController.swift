3610ae9e8dde3e6530da49f5e98792bf92eb18c4
Moi <samuel@MacBook-Pro-de-samuel.local>
2017-12-05 11:55:36 -0500

    VOKKK

--- trigo/ViewController.swift
+++ trigo/ViewController.swift
@@ -7,13 +7,33 @@
 //
 
 import UIKit
+import UIKit
+var arrayBonus = ["Boost X2        10💲":1,
+                  "FREEZE 5s       50💲":2,
+                  "SIZE BALL X2    100💲":3]
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayBonus.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.default,reuseIdentifier:nil)
+        cell.textLabel?.text = [String](arrayBonus.keys)[indexPath.row]
+        return cell
+    }
+    
+    
+   
+    @IBOutlet weak var joueur: UIImageView!
+    @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var tirer: UIButton!
     @IBOutlet weak var balle: UIView!
+    //-------TABLEAU---------
+    @IBOutlet weak var boutique: UITableView!
     
-    @IBOutlet weak var angleField: UITextField!
+    //-----------------------
+    
     
     //----point du viseur--------
     @IBOutlet weak var viseur: UIView!
@@ -31,6 +51,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var defenseur2: UIImageView!
     var originalDefenseurDroiteX: CGFloat!
     var originalDefenseurDroiteY: CGFloat!
+    @IBOutlet weak var defenseur3: UIImageView!
+    var originalDefenseurGauche2X: CGFloat!
+    var originalDefenseurGauche2Y: CGFloat!
     
     
     var cosdef: Double!
@@ -48,6 +71,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tirer.isEnabled = false
+        tirer.alpha = 0.5
         placerBall()
         placerViseur()
         placerDefenseur()
@@ -56,15 +81,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         tirer.layer.cornerRadius = 50
         balle.layer.cornerRadius = 12.5
+//        balle.layer.borderColor = (UIColor(red: 0.5, green: 0.5, blue: 0, alpha: 1.0) as! CGColor)
+        balle.layer.borderWidth = 3
         //----point du viseur--------
         viseur.layer.cornerRadius = 7.5
         viseur2.layer.cornerRadius = 7.5
         viseur3.layer.cornerRadius = 7.5
-        //-------------------------------
-        
+       
         
         
     }
+    //-----------------------------------
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -75,12 +102,20 @@ class ViewController: UIViewController {
     
     
     @IBAction func animateBall(_ sender: UIButton) {
+        
         placerBall()
         aTimer = Timer.scheduledTimer(timeInterval: 0.005, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
         
         
     }
     func doAnimation(){
+        //--- Desactivation  pour ne pas modifier la tracjectoire de la balle une fois lancer
+        tirer.isEnabled = false
+        tirer.alpha = 0.5
+        slider.isEnabled = false
+        slider.alpha = 0.5
+        joueur.transform = CGAffineTransform(rotationAngle: -0.785398); //45 degree en radian par rapport à l'épaule droite du joueur
+        //---
         distance += 1
         balle.center.x += CGFloat(cosv)
         balle.center.y += CGFloat(sinv)
@@ -89,6 +124,12 @@ class ViewController: UIViewController {
             aTimer.invalidate()
             aTimer = nil
             placerBall()
+             //--- Reactivation  pour faire un nouveau lancer
+            tirer.isEnabled = true
+            tirer.alpha = 1
+            slider.isEnabled = true
+            slider.alpha = 1
+            joueur.transform = CGAffineTransform(rotationAngle: 0);
         }
         
     }
@@ -103,6 +144,9 @@ class ViewController: UIViewController {
     }
     //-----------------VISEUR----------------
     @IBAction func trajectoire(_ sender: UISlider) {
+        tirer.isEnabled = true
+        tirer.alpha = 1
+        
         let degreesv = sender.value
         cosv = __cospi(Double(degreesv/180))
         sinv = __sinpi(Double(degreesv/180))
@@ -162,6 +206,21 @@ class ViewController: UIViewController {
             defenseur2.center.x = 500
             defenseur2.center.y = UIScreen.main.bounds.height / 4
         }
+        //-------defenseur3-----------
+        if defenseur3.center.x < UIScreen.main.bounds.width {
+            defenseur3.center.x += 2
+            defenseur3.center.y += 0
+        }else {
+            
+            defenseur3.center.x = -1
+            defenseur3.center.y = UIScreen.main.bounds.height / 4
+        }
+        
+        
+        
+        
+        
+        //----------------------------
         
         
     }
@@ -176,12 +235,18 @@ class ViewController: UIViewController {
         defenseur2.center.y = UIScreen.main.bounds.height / 4
         originalDefenseurDroiteX = viseur.center.x
         originalDefenseurDroiteY = viseur.center.y
+        //-------defenseur3------------
+        defenseur3.center.x = UIScreen.main.bounds.width - 1
+        defenseur3.center.y = UIScreen.main.bounds.height / 4
+        originalDefenseurGauche2X = viseur.center.x
+        originalDefenseurGauche2Y = viseur.center.y
+        //-------defenseur2------------
         
     }
     
     
     
-    //---------------------------------------------
+    //------------------COLLISION---------------------------
     
     
     
